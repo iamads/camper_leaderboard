e8d29c7cca0f197bd2161b75a11c75fbac265d15
ads <abhijeet3192@gmail.com>
2016-11-17 09:50:42 +0530

    Using fetch, blueprint + fetch_users + displaying users with info as table

--- src/App.js
+++ src/App.js
@@ -2,24 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'whatwg-fetch';
+import './blueprint.css';
+import * as Blueprint from "@blueprintjs/core"
+
+const alltime = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
+const recent = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
+
+function TableRow(props) {
+    const usernameStyle = {
+        verticalAlign: 'inherit',
+        marginLeft: '20px',
+    }
+    return(
+        <tr>
+            <td>{props.rank}</td>
+            <td>
+                <span><img src={props.img} height="30" width="30" /></span>
+                <span style={usernameStyle}>{props.username}</span>
+            </td>
+            <td>{props.alltime}</td>
+            <td>{props.recent}</td>
+        </tr>
+        )
+}
 
 class Table extends Component {
-    fetch_recent_leaderboard() {
-        fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+    fetch_leaderboard(type) {
+        let that = this
+         fetch(type)
             .then(function(response){
                 return response.json()
             }).then(function(json){
-                console.log('parsed json', json)
-            }).catch(function(ex){
-                console.log('parsing failed', ex)
-            })
-    }
-    fetch_alltime_leaderboard() {
-        fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
-            .then(function(response){
-                return response.json()
-            }).then(function(json){
-                console.log('parsed json', json)
+                let arr = json.map(function(json, index){
+                    return {rank: index+1, username: json.username, recent: json.recent, alltime: json.alltime, the_key: index+1, img: json.img }
+                })
+                that.setState({
+                    list: arr,
+                    url: type,
+                })
             }).catch(function(ex){
                 console.log('parsing failed', ex)
             })
@@ -27,35 +47,41 @@ class Table extends Component {
     constructor() {
         super();
         this.state = { 
-            list: [1,2,3,4]
-        }
-        this.fetch_recent_leaderboard()
+            list: [] ,
+            url: recent,
+        };
+        this.fetch_leaderboard(recent);
     }
     render() {
+        const tableStyle = {
+            width: "100%"
+        }
         return (
                 <div>
-                    <table>
-                        <tr>LeaderBoard</tr>
-                        <tr>
-                            <th>
-                                #
-                            </th>
-                            <th>
-                                Camper Name
-                            </th>
-                            <th>
-                                Points in past 30 days
-                            </th>
-                            <th>
-                                All time points
-                            </th>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>2</td>
-                            <td>3</td>
-                            <td>4</td>
-                        </tr>
+                    <table className="pt-table pt-interactive" style={tableStyle}>
+                        <thead> 
+                            <tr>
+                                <th>
+                                    #
+                                </th>
+                                <th>
+                                    Camper Name
+                                </th>
+                                <th>
+                                    Points in past 30 days
+                                </th>
+                                <th>
+                                    All time points
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.list.map(function(player_details){
+                                    return <TableRow key={player_details.the_key} rank={player_details.rank} username={player_details.username} alltime={player_details.alltime} recent={player_details.recent} img={player_details.img}/>
+                                })
+                            }
+                        </tbody>
                     </table>
                 </div>
                 )
@@ -67,13 +93,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav className="pt-navbar pt-dark">
+          <div className="pt-navbar-group pt-align-left">
+            <div className="pt-navbar-heading">Camper LeaderBoard</div>
+          </div>
+          <div className="pt-navbar-group pt-align-right">
+            <button className="pt-button pt-minimal pt-icon-home">Home</button>
+            <button className="pt-button pt-minimal pt-icon-document">Files</button>
+            <span className="pt-navbar-divider"></span>
+            <button className="pt-button pt-minimal pt-icon-user"></button>
+            <button className="pt-button pt-minimal pt-icon-notifications"></button>
+            <button className="pt-button pt-minimal pt-icon-cog"></button>
+          </div>
+        </nav>
         <Table />
       </div>
     );
